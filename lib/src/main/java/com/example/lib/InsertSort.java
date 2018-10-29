18f3ca36dff80a65dabbf6ec209d4330e4d7bb54
wenshuo001 <1443678932@qq.com>
2018-10-29 16:35:08 +0800

    插入排序

---
+++ lib/src/main/java/com/example/lib/InsertSort.java
@@ -0,0 +1,34 @@
+package com.example.lib;
+
+import java.util.Arrays;
+
+/**
+ * Created by  on 2018/10/29.
+ */
+
+public class InsertSort {
+
+     public void insertionSort(int[] a,int n){
+        if(n<=1) return;
+
+        for (int i=1;i<n;++i){
+            int value=a[i];
+            int j=i - 1;
+            //查找插入的位置
+            for(;j>=0;--j){
+                if(a[j]>value){
+                    a[j+1] = a[j];
+                }else {
+                    break;
+                }
+            }
+            a[j+1] =value; //插入数据
+        }
+
+     }
+    public static void main(String[] args){
+        int[] arr= new int[]{4,7,6,5,3,2,8,1};
+        new InsertSort().insertionSort(arr,arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+}
